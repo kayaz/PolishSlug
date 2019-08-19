911f2671a5d3b39a07adf47077aa005d4126dd64
Jacek <kayaz@tlen.pl>
2019-08-19 02:24:14 +0200

    Update function.php

--- function.php
+++ function.php
@@ -1,10 +1,10 @@
 <?php
-		function make_slug($value) {
-            $value = strtr($value, array('ą' => 'a', 'ć' => 'c', 'ę' => 'e', 'ł' => 'l', 'ń' => 'n', 'ó' => 'o', 'ś' => 's', 'ź' => 'z', 'ż' => 'z', 'Ą' => 'A', 'Ć' => 'C', 'Ę' => 'E', 'Ł' => 'L', 'Ń' => 'N', 'Ó' => 'O', 'Ś' => 'S', 'Ź' => 'Z', 'Ż' => 'Z'));
-            $value = str_replace(' ', '-', trim($value));
-            $value = preg_replace('/[^a-zA-Z0-9\-_]/', '', (string) $value);
-            $value = preg_replace('/[\-]+/', '-', $value);
-            $value = stripslashes($value);
-            return urlencode(strtolower($value));
-		}
+	function make_slug($value) {
+		$value = strtr($value, array('ą' => 'a', 'ć' => 'c', 'ę' => 'e', 'ł' => 'l', 'ń' => 'n', 'ó' => 'o', 'ś' => 's', 'ź' => 'z', 'ż' => 'z', 'Ą' => 'A', 'Ć' => 'C', 'Ę' => 'E', 'Ł' => 'L', 'Ń' => 'N', 'Ó' => 'O', 'Ś' => 'S', 'Ź' => 'Z', 'Ż' => 'Z'));
+		$value = str_replace(' ', '-', trim($value));
+		$value = preg_replace('/[^a-zA-Z0-9\-_]/', '', (string) $value);
+		$value = preg_replace('/[\-]+/', '-', $value);
+		$value = stripslashes($value);
+		return urlencode(strtolower($value));
+	}
 ?>
